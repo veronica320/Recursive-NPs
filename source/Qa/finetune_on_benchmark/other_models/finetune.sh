7f0bf5aed81db168a019b4d9f539110c462c52c8
veronica320 <lyuqing@sas.upenn.edu>
2022-05-08 15:23:48 -0400

    Bug fix in finetuning format

--- source/Qa/finetune_on_benchmark/other_models/finetune.sh
+++ source/Qa/finetune_on_benchmark/other_models/finetune.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 ## Finetune baselines on MPE
-nohup python finetune_te.py --mode train_eval --task MNLI --target MPE --model bert --cuda 0 > logs/mpe_bert.log 2>&1 &
-nohup python finetune_te.py --mode train_eval --task MNLI --target MPE --model bert-l --cuda 1 > logs/mpe_bert-l.log 2>&1 &
-nohup python finetune_te.py --mode train_eval --task MNLI --target MPE --model roberta --cuda 2 > logs/mpe_roberta.log 2>&1 &
-nohup python finetune_te.py --mode test --task MNLI --target MPE --model roberta-l --cuda 6 > logs/mpe_robertal_test.log 2>&1 &
-
-## Finetune baselines on ADEPT
-nohup python finetune_te.py --mode train_eval --task WNLI --target ADEPT --model bert --cuda 0 > logs/adept_bert.log 2>&1 &
-nohup python finetune_te.py --mode train_eval --task WNLI --target ADEPT --model bert-l --cuda 1 > logs/adept_bert-l.log 2>&1 &
-nohup python finetune_te.py --mode train_eval --task WNLI --target ADEPT --model roberta --cuda 2 > logs/adept_roberta.log 2>&1 &
-nohup python finetune_te.py --mode train_eval --task WNLI --target ADEPT --model roberta-l --cuda 3 > logs/adept_roberta-l.log 2>&1 &
+nohup python finetune_te.py --mode train_eval --task WNLI --target MPE --model bert --cuda 4 > logs/mpe_bert.log 2>&1 &
+#nohup python finetune_te.py --mode train_eval --task WNLI --target MPE --model bert-l --cuda 5 > logs/mpe_bert-l.log 2>&1 &
+#nohup python finetune_te.py --mode train_eval --task WNLI --target MPE --model roberta --cuda 6 > logs/mpe_roberta.log 2>&1 &
+#nohup python finetune_te.py --mode test --task WNLI --target MPE --model roberta-l --cuda 7 > logs/mpe_robertal_test.log 2>&1 &
+#
+### Finetune baselines on ADEPT
+#nohup python finetune_te.py --mode train_eval --task WNLI --target ADEPT --model bert --cuda 4 > logs/adept_bert.log 2>&1 &
+#nohup python finetune_te.py --mode train_eval --task WNLI --target ADEPT --model bert-l --cuda 5 > logs/adept_bert-l.log 2>&1 &
+#nohup python finetune_te.py --mode train_eval --task WNLI --target ADEPT --model roberta --cuda 6 > logs/adept_roberta.log 2>&1 &
+#nohup python finetune_te.py --mode train_eval --task WNLI --target ADEPT --model roberta-l --cuda 7 > logs/adept_roberta-l.log 2>&1 &
 
